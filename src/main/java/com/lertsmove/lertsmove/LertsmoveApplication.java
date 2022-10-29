b1f03a8d25dcad3f11ca4ede73b30e0d0b33a01f
btatenov <btatenov@nurtelecom.kg>
2022-10-29 19:45:33 +0600

    Initial commit

---
+++ src/main/java/com/lertsmove/lertsmove/LertsmoveApplication.java
@@ -0,0 +1,13 @@
+package com.lertsmove.lertsmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LertsmoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LertsmoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lertsmove/lertsmove/LertsmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lertsmove.lertsmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LertsmoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
