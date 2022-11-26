c63adb27b477ea41b5f0f6f85d868356a498acb9
Diyaz007 <diyaz18.08@gmail.com>
2022-11-26 16:27:56 +0600

    addCity

--- src/main/java/com/letsmove/controller/CityController.java
+++ src/main/java/com/letsmove/controller/CityController.java
@@ -1,12 +1,37 @@
 package com.letsmove.controller;
 
+import com.letsmove.entity.City;
+import com.letsmove.entity.Users;
 import com.letsmove.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class CityController {
     @Autowired
     private CityService cityService;
+
+    @RequestMapping(value = "/add_city", method = RequestMethod.GET)
+    public ModelAndView addCity() {
+        ModelAndView modelAndView = new ModelAndView("addCity");
+        modelAndView.addObject("city", new City());
+        return modelAndView;
+    }
+
+
+    @PostMapping(value = "/save_city")
+    public String registration(@ModelAttribute(name = "city") City city) {
+        try {
+            this.cityService.save(city);
+            return "adminMain";
+        }catch (Exception e){
+            return "addCity";
+        }
+    }
 }
--- src/main/java/com/letsmove/service/CityService.java
+++ src/main/java/com/letsmove/service/CityService.java
@@ -1,13 +1,21 @@
 package com.letsmove.service;
 
 import com.letsmove.dao.CityRepository;
+import com.letsmove.entity.City;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 
 @Service
 public class CityService{
     @Autowired
     private CityRepository cityRepository;
+
+    public City save(City city){
+        city.setCreatedDate(new Date());
+        return cityRepository.save(city);
+    }
 
 }
